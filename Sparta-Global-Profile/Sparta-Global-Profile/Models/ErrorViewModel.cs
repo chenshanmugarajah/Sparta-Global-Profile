541156fd37102e0222a3974d8cc4eff644e85203
cmjnorman <cmjnorman@gmail.com>
2020-08-28 10:43:36 +0100

    Initial Commit

---
+++ Sparta-Global-Profile/Sparta-Global-Profile/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sparta_Global_Profile.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
