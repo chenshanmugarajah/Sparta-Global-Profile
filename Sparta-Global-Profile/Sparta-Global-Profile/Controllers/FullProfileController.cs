bd34c9600674f784aeb8baafe51d21b71fccf841
chen shanmugarajah <chenshanmugarajah@gmail.com>
2020-09-01 11:24:59 +0100

    added viewmodel for full profile

--- Sparta-Global-Profile/Sparta-Global-Profile/Controllers/FullProfileController.cs
+++ Sparta-Global-Profile/Sparta-Global-Profile/Controllers/FullProfileController.cs
@@ -13,12 +13,32 @@ namespace Sparta_Global_Profile.Controllers
     {
         public IActionResult Index()
         {
-            SpartaGlobalProfileDbContext spartaGlobal = new SpartaGlobalProfileDbContext();
-            var myModel = new FullProfile();
-            myModel.Users = spartaGlobal.Users.ToList();
-            myModel.Profiles = spartaGlobal.Profiles.ToList();
-            ViewBag.Users = new SelectList(spartaGlobal.Users.ToList());
-            return View(myModel);
+            SpartaGlobalProfileDbContext _context = new SpartaGlobalProfileDbContext();
+
+            int TESTUSERID = 1; // session
+
+            User currentUser = _context.Users.Where(u => u.UserId == TESTUSERID).FirstOrDefault();
+            Profile profile = _context.Profiles.Where(p => p.UserId == currentUser.ProfileId).FirstOrDefault();
+            FullProfile fullProfile = new FullProfile();
+
+            fullProfile.Assignments = _context.Assignments.Where(a => a.ProfileId == profile.ProfileId).ToList();
+            fullProfile.Certifications = _context.Certifications.Where(a => a.ProfileId == profile.ProfileId).ToList();
+            fullProfile.Comments = _context.Comments.Where(a => a.ProfileId == profile.ProfileId).ToList();
+            fullProfile.Educations = _context.Educations.Where(a => a.ProfileId == profile.ProfileId).ToList();
+            fullProfile.Employments = _context.Employment.Where(a => a.ProfileId == profile.ProfileId).ToList();
+            fullProfile.Hobbies = _context.Hobbies.Where(a => a.ProfileId == profile.ProfileId).ToList();
+
+            fullProfile.ProfileAcademyStatus = profile.StatusId;
+            fullProfile.ProfileApprovalStatus = profile.Approved;
+            fullProfile.ProfileFullName = profile.ProfileName;
+            fullProfile.ProfilePic = profile.ProfilePicture;
+            // profile stream ?
+            fullProfile.ProfileStream = "C# Dev";
+            fullProfile.ProfileSummary = profile.Summary;
+            fullProfile.Skills = profile.Skills.ToList();
+            fullProfile.SpartaProjects = profile.SpartaProjects.ToList();
+
+            return View(fullProfile);
         }
     }
 }
--- Sparta-Global-Profile/Sparta-Global-Profile/ViewModel/FullProfile.cs
+++ Sparta-Global-Profile/Sparta-Global-Profile/ViewModel/FullProfile.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Sparta_Global_Profile.Models;
 
@@ -9,9 +10,22 @@ namespace Sparta_Global_Profile.ViewModel
 {
     public class FullProfile
     {
-        public IEnumerable<User> Users { get; set; }
-        public IEnumerable<Profile> Profiles { get; set; }
-
+        public int ProfileUserId { get; set; }
+        public string ProfilePic { get; set; }
+        public string ProfileFullName { get; set; }
+        public string ProfileSummary { get; set; }
+        public string ProfileStream { get; set; } // fk
+        public bool ProfileApprovalStatus { get; set; }
+        public int ProfileAcademyStatus { get; set; } // fk
         
+        public IEnumerable<Certification> Certifications { get; set; }
+        public IEnumerable<Assignment> Assignments { get; set; }
+        public IEnumerable<Skill> Skills { get; set; }
+        public IEnumerable<Comment> Comments { get; set; }
+        public IEnumerable<Education> Educations { get; set; }
+        public IEnumerable<Employment> Employments { get; set; }
+        public IEnumerable<Hobby> Hobbies { get; set; }
+        public IEnumerable<SpartaProject> SpartaProjects { get; set; }
+
     }
 }
