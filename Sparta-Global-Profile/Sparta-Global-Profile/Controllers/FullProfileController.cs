37bfb84c2072573aa6c6af49b35c0690193a4104
chen shanmugarajah <chenshanmugarajah@gmail.com>
2020-09-01 16:30:41 +0100

    added edit view

--- Sparta-Global-Profile/Sparta-Global-Profile/Controllers/FullProfileController.cs
+++ Sparta-Global-Profile/Sparta-Global-Profile/Controllers/FullProfileController.cs
@@ -48,23 +48,40 @@ namespace Sparta_Global_Profile.Controllers
             return View(fullProfile);
         }
 
-        // POST: Profile/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ProfileId,UserId,StatusId,ProfileName,ProfilePicture,Summary,CourseId,Approved")] Profile profile)
+        public IActionResult Edit()
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(profile);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseId", profile.CourseId);
-            ViewData["StatusId"] = new SelectList(_context.Status, "StatusId", "StatusId", profile.StatusId);
-            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", profile.UserId);
-            return View(profile);
+
+            int TESTUSERID = 1; // session
+
+            User currentUser = _context.Users.Where(u => u.UserId == TESTUSERID).FirstOrDefault();
+
+            Profile profile = _context.Profiles.Where(p => p.UserId == currentUser.UserId).FirstOrDefault();
+
+            //Profile profile = _context.Profiles.FirstOrDefault();
+
+            FullProfile fullProfile = new FullProfile();
+
+            fullProfile.ProfileAcademyStatus = profile.StatusId;
+            fullProfile.ProfileApprovalStatus = profile.Approved;
+            fullProfile.ProfileFullName = profile.ProfileName;
+            fullProfile.ProfilePic = profile.ProfilePicture;
+
+            fullProfile.Assignments = _context.Assignments.Where(a => a.ProfileId == profile.ProfileId).ToList();
+            fullProfile.Certifications = _context.Certifications.Where(a => a.ProfileId == profile.ProfileId).ToList();
+            fullProfile.Comments = _context.Comments.Where(a => a.ProfileId == profile.ProfileId).ToList();
+            fullProfile.Educations = _context.Educations.Where(a => a.ProfileId == profile.ProfileId).ToList();
+            fullProfile.Employments = _context.Employment.Where(a => a.ProfileId == profile.ProfileId).ToList();
+            fullProfile.Hobbies = _context.Hobbies.Where(a => a.ProfileId == profile.ProfileId).ToList();
+
+            // profile stream ?
+            fullProfile.ProfileStream = "C# Dev";
+            fullProfile.ProfileSummary = profile.Summary;
+
+            fullProfile.Skills = _context.Skills.Where(a => a.ProfileId == profile.ProfileId).ToList();
+
+            fullProfile.SpartaProjects = _context.SpartaProjects.Where(a => a.ProfileId == profile.ProfileId).ToList();
+
+            return View(fullProfile);
         }
     }
 }
