0b2e9dd5a7c85f7319aae12afa2cba5fe44319d7
cmjnorman <cmjnorman@gmail.com>
2020-09-02 10:27:30 +0100

    Fixed login error and seperated email and password checks, added new seeds for all user types.

--- Sparta-Global-Profile/Sparta-Global-Profile/Migrations/20200902092625_AddNewUserTypeSeeds.Designer.cs
+++ Sparta-Global-Profile/Sparta-Global-Profile/Migrations/20200902092625_AddNewUserTypeSeeds.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sparta_Global_Profile.Models;
 
 namespace Sparta_Global_Profile.Migrations
 {
     [DbContext(typeof(SpartaGlobalProfileDbContext))]
-    partial class SpartaGlobalProfileDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200902092625_AddNewUserTypeSeeds")]
+    partial class AddNewUserTypeSeeds
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
